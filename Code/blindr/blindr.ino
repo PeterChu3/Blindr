68904bfe8e9ada01611935e6f827e926e7c5be00
Sam Roquitte <sam@roquitte.com>
2021-12-12 17:28:47 -0500

    Add BMP280 code

--- Code/blindr/blindr.ino
+++ Code/blindr/blindr.ino
@@ -1,5 +1,15 @@
 #include <ESP8266WebServer.h>
 
+// General config
+#define DEBUG 1
+// #define BMP280 1
+
+#ifdef BMP280
+  #include <Adafruit_BMP280.h>
+  #include <Wire.h>
+  Adafruit_BMP280 bmp;
+#endif
+
 // Wifi config
 #define HTTP_REST_PORT 80
 const char *ssid = "ssid";
@@ -7,21 +17,22 @@ const char *password = "pass";
 const String deviceName = "Blindr";
 ESP8266WebServer server(HTTP_REST_PORT);
 
+// Blind rotator config
 const int GEAR_RATIO = 2;               // Gear ratio of the blind gear to motor (2:1)
 const int NUM_ENCODER_HOLES = 10;       // Number of holes in the encoder wheel (10 = 1 rotation)
 const double NUM_WAND_ROTATIONS = 1.5;  // Number of wand rotations needed to move from one state to another
 
+// Pinout
 #define UP D6
 #define DOWN D7
 #define LED0 D5
 #define LED1 D8
 #define LED2 D4
 
+// State constants
 #define CLOSED_UP 0
 #define OPEN 1
 #define CLOSED_DOWN 2
-
-#define DEBUG 1
 
 // Can be open (blinds horizontal) or closed (blinds vertical)
 // Closed can be up or down (concave up or concave down)
@@ -32,8 +43,8 @@ const int analogInPin = A0;   // Sensor pin
 int sensorValue = 0;          // Value read from the sensor
 bool wasLow = false;          // Was the sensor low last time we checked?
 
-int ticks = 0;               // Number of ticks since last movement
-int tickStop = 0;            // Number of ticks to stop motor at
+int ticks = 0;                // Number of ticks since last movement
+int tickStop = 0;             // Number of ticks to stop motor at
 void setup() {
   #ifdef DEBUG
     Serial.begin(115200);
@@ -43,6 +54,22 @@ void setup() {
   pinMode(LED0, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
+
+  #ifdef BMP280
+    // Initialize BMP280
+    if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
+      #ifdef DEBUG
+        Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+      #endif
+      while (1) delay(10);
+    }
+
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode.
+                    Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling = 2x
+                    Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling = 16x
+                    Adafruit_BMP280::FILTER_X16,      // Filtering = 16x
+                    Adafruit_BMP280::STANDBY_MS_500); // Standby time = 500ms
+  #endif
 
   // Webserver init
   #ifdef DEBUG
@@ -124,6 +151,13 @@ void handleRoot() {
 void configureRouter() {
   server.on("/", HTTP_GET, getState);
   server.on("/", HTTP_POST, changeState);
+  #ifdef BMP280
+    server.on("/temperature", HTTP_GET, getTemperature);
+    server.on("/pressure", HTTP_GET, getPressure);
+    // Get raw values (no units) for the server
+    server.on("/temperatureRaw", HTTP_GET, getTemperatureRaw);
+    server.on("/pressureRaw", HTTP_GET, getPressureRaw);
+  #endif
 }
 
 // HTTP server helpers
@@ -133,10 +167,11 @@ String getStateText() {
 String getStateText(int state) {
   return state == OPEN ? "open" : state == CLOSED_UP ? "closed-up" : "closed-down";
 }
+
+// REST route handlers
 void getState() {
   server.send(200, "text/plain", getStateText());
 }
-
 void changeState() {
   String post_body = server.arg("plain");
   // Ensure the body is correct
@@ -162,6 +197,20 @@ void changeState() {
   String res = getStateText(currentState) + " -> " + getStateText(newState);
   server.send(200, "text/html", res);
 }
+#ifdef BMP280
+  void getTemperature() {
+    server.send(200, "text/plain", String(bmp.readTemperature()) + "°C");
+  }
+  void getPressure() {
+    server.send(200, "text/plain", String(bmp.readPressure()) + "Pa");
+  }
+  void getTemperatureRaw() {
+    server.send(200, "text/plain", String(bmp.readTemperature()));
+  }
+  void getPressureRaw() {
+    server.send(200, "text/plain", String(bmp.readPressure()));
+  }
+#endif
 
 // LED helpers
 void changeLED(int state, bool ison) {
